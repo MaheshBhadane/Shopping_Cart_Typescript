55761a6103ff0357e14aa9e3297a202e59443a0f
MaheshBhadane <mahesh.bhadane@torinit.ca>
2022-09-21 10:47:16 +0530

    changes in Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Hero1 from "../../assets/Hero1.png";
-import { Grid } from "@mui/material";
+import { CardMedia, Grid } from "@mui/material";
 
 const Hero = () => {
   return (
     <>
       <Grid container marginY={2} marginX={3}>
-        <img src={Hero1} alt="" width="1470" height="500" />
+        <CardMedia
+          style={{ height: "500px", width: "1470px" }}
+          component="img"
+          image={Hero1}
+          title="Hero Image"
+          alt="Pancakes"
+        />
+        <div
+          style={{
+            position: "absolute",
+            color: "white",
+            top: 50,
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <h1>Shopping Card</h1>{" "}
+        </div>
       </Grid>
     </>
   );
